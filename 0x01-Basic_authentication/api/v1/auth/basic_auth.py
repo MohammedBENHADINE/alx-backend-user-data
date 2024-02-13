d5238bcf9347109f9d60fbad3460fa9f72527959
Mohammed BENHADINE <mohammedbenhadine@gmail.com>
2024-02-13 16:39:00 +0100

    decode base64

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,8 @@
 """
 from api.v1.auth.auth import Auth
 from typing import List, TypeVar
+import binascii
+import base64
 
 
 class BasicAuth(Auth):
@@ -22,3 +24,19 @@ class BasicAuth(Auth):
             return None
         else:
             return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str
+            ) -> str:
+        """decode the tocken
+        """
+        if base64_authorization_header is None:
+            return None
+        elif type(base64_authorization_header) is not str:
+            return None
+        else:
+            try:
+                decoded_bytes = base64.b64decode(base64_authorization_header)
+                return decoded_bytes.decode('utf-8')
+            except binascii.Error:
+                return None
